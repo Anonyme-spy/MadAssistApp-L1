61627cf84bacd4874ea8033d70dbeffc60b5168b
Anonymespy <morgan.jonalson05@gmail.com>
2025-08-17 21:37:02 +0100

    Prepare for EAS build

--- components/AboutCard.tsx
+++ components/AboutCard.tsx
@@ -110,7 +110,7 @@ export const AboutCard = (): React.ReactElement => {
           styles.version,
           { color: theme === 'dark' ? '#666' : '#999' }
         ]}>
-          {t('about.version') || 'Version'} 1.0.0
+          {t('about.version') || 'Version'} 0.2.0
         </Text>
       </View>
     </Card>
